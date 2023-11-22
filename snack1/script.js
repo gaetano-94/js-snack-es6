9d9ff37c04a882fa67ab636e03210b655635b225
gaetano-94 <gaetano.panico1994@gmail.com>
2023-11-22 14:15:40 +0100

    feat: stampa bici più leggera

--- snack1/script.js
+++ snack1/script.js
@@ -11,19 +11,24 @@ Stampare a schermo la bici con peso minore utilizzando destructuring e template 
 const biciclettaDaCorsa = [
   {
     nome: 'Bianchi',
-    peso: 7,
+    peso: 9,
   },
   {
     nome: 'Trek',
-    peso: 6,
+    peso: 7,
   },
   {
     nome: 'Racer',
-    peso: 3,
+    peso: 5,
   },
 ];
 
 //trovare la bici con il peso minore
 const biciLeggera = biciclettaDaCorsa.reduce((minBici, bici) =>
   bici.peso < minBici.peso ? bici : minBici
+);
+
+//stampare la bici piu leggera
+console.log(
+  `la bici piu leggera e ${biciLeggera.nome} con un peso di ${biciLeggera.peso} kg`
 );
