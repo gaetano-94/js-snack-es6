6448a17a1ccdb7e38bdc192c8cc62734368f881c
gaetano-94 <gaetano.panico1994@gmail.com>
2023-11-22 14:00:54 +0100

    feat: array di bici

--- snack1/script.js
+++ snack1/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+/*
+Snack 1
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
+*/
+
+//array delle bici
+const biciclettaDaCorsa = [
+  {
+    nome: 'Bianchi',
+    peso: 7,
+  },
+  {
+    nome: 'Trek',
+    peso: 6,
+  },
+  {
+    nome: 'Racer',
+    peso: 3,
+  },
+];
